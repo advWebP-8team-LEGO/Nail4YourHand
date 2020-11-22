ac78176fc6d3b1fab50179de7d27b510ebc1262c
안준영 <ajyng@anjun-yeong-ui-MacBookPro.local>
2020-11-23 01:17:46 +0900

    회원가입 구현

--- NFYH/routes/post.js
+++ NFYH/routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const postController = require('../../controller/postController');
+const postController = require('../controller/postController');
 
 // [POST] localhost:3000/posts
 router.post('/', postController.createPost);
@@ -8,7 +8,7 @@ router.post('/', postController.createPost);
 // [GET] localhost:3000/posts
 router.get('/', postController.readPosts);
 
-// [POST] localhost:3000/posts/:postId/like
+// [POST] localhost:3000/post/:postId/like
 router.post('/:postId/like', postController.createLike);
 
 module.exports = router;
